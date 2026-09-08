82856c8b4386d67e518ca163cb1d7993ff076450
김동환 <whans1996@naver.com>
2026-09-08 15:43:42 +0900

    [Passed] 세 실수의 반올림 (170ms, 12MB)

---
+++ trail1/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        double a= sc.nextFloat();
+        double b =sc.nextFloat();
+        double c =sc.nextDouble();
+        
+        System.out.printf("%.3f",a);
+        System.out.println();
+        System.out.printf("%.3f",b);
+        System.out.println();
+        System.out.printf("%.3f",c);
+        System.out.println();
+    }
+}
