61edbb9e108a86bef8bbdb2c89ed6e828fe36acf
김동환 <whans1996@naver.com>
2026-09-09 08:54:35 +0900

    [Passed] 날짜 변경하여 출력 2 (122ms, 10MB)

---
+++ trail1/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        
+        String[] arr = a.split("-");
+        
+        System.out.println(arr[2]+"."+arr[0]+"."+arr[1]);
+    }
+}
