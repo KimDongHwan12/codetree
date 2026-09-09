19758a3028ebe5ecb497f848dcc43ffb08825fc5
김동환 <whans1996@naver.com>
2026-09-09 17:48:20 +0900

    [Passed] 두 개의 직각삼각형 (128ms, 10MB)

---
+++ trail1/두 개의 직각삼각형/two-right-triangle.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+
+            // 왼쪽 별
+            for (int j = 0; j < N - i; j++) {
+                System.out.print("*");
+            }
+
+            // 가운데 공백
+            for (int j = 0; j < 2 * i; j++) {
+                System.out.print(" ");
+            }
+
+            // 오른쪽 별
+            for (int j = 0; j < N - i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
