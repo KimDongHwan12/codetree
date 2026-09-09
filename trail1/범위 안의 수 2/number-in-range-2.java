0bdc163e8cadc1971f0958563758aab218a742ba
김동환 <whans1996@naver.com>
2026-09-09 15:00:04 +0900

    [Passed] 범위 안의 수 2 (143ms, 10MB)

---
+++ trail1/범위 안의 수 2/number-in-range-2.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        int[] arr = new int[10];
+        
+        int sum = 0;
+        int count = 0;
+        
+        for(int i = 0; i<10; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i]>=0 && arr[i]<=200) {
+                count++;
+                sum+=arr[i];
+            }
+        }
+        System.out.printf("%d %.1f", sum, (double)sum/count);
+    }
+}
