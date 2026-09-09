1e7d3b5963e659a50c37d5efbf9874a87872aef0
김동환 <whans1996@naver.com>
2026-09-09 14:03:03 +0900

    [Passed] 특정 조건 세 정수 비교 (136ms, 10MB)

---
+++ trail1/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // a가 최솟값인지
+        if (a <= b && a <= c) {
+            System.out.print(1 + " ");
+        } else {
+            System.out.print(0 + " ");
+        }
+
+        // 세 수가 모두 같은지
+        if (a == b && b == c) {
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+    }
+}
