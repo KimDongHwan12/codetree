5354d8a865ed57d54ca12ed3515fbc1b6a585873
김동환 <whans1996@naver.com>
2026-09-09 14:48:40 +0900

    [Passed] 5의 배수의 합 (129ms, 10MB)

---
+++ trail1/5의 배수의 합/sum-of-specific-condition-multiple.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int c = 0;
+        int count = 0;
+        
+        if(a<b) {
+            for(int i =a ; i<=b; i++) {
+                
+                if(i%5==0) {
+                    c+=i;
+                    
+                }
+            }
+        }else {
+            for(int i =b ; i<=a; i++) {
+                
+                if(i%5==0) {
+                    c+=i;
+                    
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}
