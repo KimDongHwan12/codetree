e459fc62904e2c898f3fcb97a0ee69818cd42fd2
김동환 <whans1996@naver.com>
2026-09-10 08:44:10 +0900

    [Passed] 특정 위치의 문자 (119ms, 10MB)

---
+++ trail1/특정 위치의 문자/char-in-specific-location.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char ch = sc.next().charAt(0);
+
+        char[] arr = {'L', 'E', 'B', 'R', 'O', 'S'};
+
+        int index = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == ch) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            System.out.println("None");
+        } else {
+            System.out.println(index);
+        }
+    }
+}
