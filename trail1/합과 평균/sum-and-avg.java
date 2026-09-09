51be62a95d8a09014804cdc74dbb199d6c38cd46
김동환 <whans1996@naver.com>
2026-09-09 09:13:13 +0900

    [Passed] 합과 평균 (138ms, 10MB)

---
+++ trail1/합과 평균/sum-and-avg.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int c= a+b;
+        double d = (double) c / 2;
+        
+        System.out.printf("%d %.1f", c, d);
+    }
+}
