cdeaf10154bcc326d7331f27074352b9b31eceff
김동환 <whans1996@naver.com>
2026-09-09 14:40:12 +0900

    [Passed] 등장횟수 확인 (132ms, 10MB)

---
+++ trail1/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        
+        int[] arr = new int[5];
+        int cnt = 0;
+        
+        for(int i = 0; i<arr.length ; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] % 2 ==0) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
+
