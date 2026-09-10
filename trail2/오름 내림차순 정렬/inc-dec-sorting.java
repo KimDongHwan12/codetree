406788a098168b9d3f569de00fadd636e603b2a3
김동환 <whans1996@naver.com>
2026-09-10 11:16:37 +0900

    [Passed] 오름 내림차순 정렬 (147ms, 10MB)

---
+++ trail2/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Integer[] arr = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 오름차순
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+
+        // 내림차순
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
