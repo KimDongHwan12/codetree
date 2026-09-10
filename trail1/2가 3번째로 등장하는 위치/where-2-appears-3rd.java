a42a99f32cdac80d8d72a8c410574a19d5d35a98
김동환 <whans1996@naver.com>
2026-09-10 09:02:06 +0900

    [Passed] 2가 3번째로 등장하는 위치 (138ms, 10MB)

---
+++ trail1/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int[] arr =new int [n+1];
+        
+        int count = 0;
+        
+        for(int i = 1; i<=n ;i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] == 2) {
+                count++;
+                if(count ==3) {
+                    System.out.println(i);
+                }
+            }
+        }
+        
+    }
+}
