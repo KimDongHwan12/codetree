29d4d5dfa014262a7059182718a350d2ae69b016
김동환 <whans1996@naver.com>
2026-09-10 11:35:10 +0900

    [Passed] 2개씩 그룹짓기 (215ms, 13MB)

---
+++ trail2/2개씩 그룹짓기/group-of-pairs.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[2 * n];
+        for (int i = 0; i < 2 * n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(nums);
+        int max = 0;
+        for (int i = 0; i < 2 * n; i++) {
+            int sum = nums[i] + nums[2*n-1-i];
+            
+            if(sum >max) {
+                max = sum;
+            }
+        }
+        System.out.println(max);
+    }
+}
