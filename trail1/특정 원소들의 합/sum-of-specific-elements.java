a99efa104e8adb6a6979d77c4b0061628db689c0
김동환 <whans1996@naver.com>
2026-09-10 10:30:10 +0900

    [Passed] 특정 원소들의 합 (124ms, 10MB)

---
+++ trail1/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[4][4];
+        int sum = 0;
+
+        // 배열 입력
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        // 색칠된 부분의 합
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum += arr[i][j];
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
