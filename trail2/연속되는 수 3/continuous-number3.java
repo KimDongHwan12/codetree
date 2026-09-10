7e7eb5af7e91732a78b387dd1e0b7e416f573990
김동환 <whans1996@naver.com>
2026-09-10 15:18:17 +0900

    [Passed] 연속되는 수 3 (200ms, 13MB)

---
+++ trail2/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        
+        int count = 1;
+        int max = 1;
+        
+        for(int i = 0; i<n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        for(int i = 1; i<n ; i++) {
+            
+            if(arr[i] <0 && arr[i-1] <0) {
+                count++;
+            }else if(arr[i] >0 && arr[i-1] >0) {
+                count++;
+            }else {
+                count = 1;
+            }
+            if (count > max) {
+                max = count;
+            }
+        }
+        System.out.println(max);
+    }
+}
