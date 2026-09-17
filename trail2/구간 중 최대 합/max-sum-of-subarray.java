6282eab2c9121ad0b7bdf762e6b46e663ce413ab
김동환 <whans1996@naver.com>
2026-09-17 10:59:11 +0900

    [Passed] 구간 중 최대 합 (138ms, 10MB)

---
+++ trail2/구간 중 최대 합/max-sum-of-subarray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        int result = 0;
+        for (int i = 0; i <= n-k; i++) {
+            int sum = arr[i];
+            for(int j = i+1; j<i+k;j++) {
+                sum += arr[j];
+            }
+            result = Math.max(sum, result);
+        }
+        System.out.println(result);
+    }
+}
