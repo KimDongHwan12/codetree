f3693f977c9a0c6798a1d059ba1dca899e481591
김동환 <whans1996@naver.com>
2026-09-17 09:14:34 +0900

    [Passed] 일렬로 서있는 소 2 (147ms, 10MB)

--- trail2/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ trail2/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -1,35 +1,41 @@
-import java.util.Scanner;
-/**
- * 입력
- * N
- * N마리의 소
+/**구할해야할 것: i<j<k와 범위가 조건에 맞는 것
  * 
- * 어떤 조건에 따라 정렬하여 조건이 맞는걸 보기
+ * 입력에서 주어지는 것: 소의 마리수, 소의 키
+ * 
+ * 내가 선택해야하는 것: 시작소, 다음소, 마지막소
+ * 
+ * 선택범위 : 시작소 = 전체에서 -2 다음소는 시작소+1 마지막소는 시작소 +2
+ * 
+ * 문제 조건: 조건이 문제에서 대놓고 주어짐
  */
+
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        
+       
         int n = sc.nextInt();
         
         int[] arr = new int[n];
         
-        for(int i = 0; i<n ; i++) {
+        for(int i = 0; i<n; i++) {
             arr[i] = sc.nextInt();
         }
-        int count = 0;
         
-        for(int i = 0; i<n; i++) {
-            for(int j = i+1; j<n; j++) {
-                for(int k = j+1 ; k<n; k++) {
+        int result = 0;
+        
+        for(int i = 0; i<n-2; i++) {
+            int count = 0;
+            for(int j = i+1; j<n-1; j++ ) {
+                for(int k = j+1; k<n; k++) {
                     
-                    if((arr[i]<=arr[j] && arr[j]<=arr[k]) &&
-                            (i<j && j<k)) {
+                    if(arr[i]<= arr[j] && arr[j]<=arr[k]) {
                         count++;
                     }
                 }
             }
+            result += count;
         }
-        System.out.println(count);
+        System.out.println(result);
     }
 }
