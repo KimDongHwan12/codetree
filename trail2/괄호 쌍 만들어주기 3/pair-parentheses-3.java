1f7f9f037753762974e0f4161425c17db3178419
김동환 <whans1996@naver.com>
2026-09-17 09:05:23 +0900

    [Passed] 괄호 쌍 만들어주기 3 (137ms, 10MB)

--- trail2/괄호 쌍 만들어주기 3/pair-parentheses-3.java
+++ trail2/괄호 쌍 만들어주기 3/pair-parentheses-3.java
@@ -1,27 +1,29 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-
-        String A = sc.next();
-
-        long open = 0;
-        long answer = 0;
-
-        for (int i = 0; i < A.length(); i++) {
-
-            char c = A.charAt(i);
-
-            if (c == '(') {
-                open++;
-            } else {
-                answer += open;
-            }
-        }
-
-        System.out.println(answer);
+       
+       
+       String str = sc.next();
+       int result = 0;
+       
+       for(int i = 0; i<str.length()-1; i++) {
+           char chr = str.charAt(i);
+           boolean open = false;
+           int count = 0;
+           if(chr == ')') {
+               continue;
+           }else {
+               open = true;
+           }
+           for(int j = i+1; j<str.length(); j++) {
+               char chr2 = str.charAt(j);
+               if(open && chr2 == ')') {
+                   count++;
+               }
+           }
+           result += count;
+       }
+       System.out.println(result);
     }
 }
